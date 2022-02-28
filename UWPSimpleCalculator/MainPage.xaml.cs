8a12ff741f9a10adabf759bccd7aac243f697c4f
Влад Богданець <vbohdanets03@gmail.com>
2022-02-28 13:01:37 +0200

    Upgrade/Refactoring

--- UWPSimpleCalculator/MainPage.xaml.cs
+++ UWPSimpleCalculator/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,42 +23,79 @@ namespace UWPSimpleCalculator
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        static Operations operations = new Operations();
-        bool ch;
+        private bool status;
 
         public MainPage()
         {
             this.InitializeComponent();
         }
 
+        private async void ShowMessage(string str)
+        {
+            var dialog = new MessageDialog(str);
+            await dialog.ShowAsync();
+        }
 
         private void btn_Add_Click(object sender, RoutedEventArgs e)
         {
-            if (!ch) textBox2.Text = operations.Add(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
-            else
+            try
             {
-                textBox2.Text = operations.PercentageAdd(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
-                ch = false;
+                if(textBox.Text == string.Empty || textBox.Text == string.Empty)
+                {
+                    ShowMessage("Please enter data!");
+                }
+                else if (!status) textBox2.Text = MathOp.Add(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+                else
+                {
+                    textBox2.Text = MathOp.PercentageAdd(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+                    status = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                ShowMessage(ex.Message);
             }
         }
 
         private void btn_Sub_Click(object sender, RoutedEventArgs e)
         {
-            if (!ch) textBox2.Text = operations.Subtract(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
-            else
+            try
             {
-                textBox2.Text = operations.PercentageSub(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
-                ch = false;
+                if (textBox.Text == string.Empty || textBox.Text == string.Empty)
+                {
+                    ShowMessage("Please enter data!");
+                }
+                else if (!status) textBox2.Text = MathOp.Subtract(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+                else
+                {
+                    textBox2.Text = MathOp.PercentageSub(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+                    status = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
             }
         }
 
         private void btn_Mult_Click(object sender, RoutedEventArgs e)
         {
-            if (!ch) textBox2.Text = operations.Multiply(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
-            else
+            try
             {
-                textBox2.Text = operations.PercentageMult(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
-                ch = false;
+                if (textBox.Text == string.Empty || textBox.Text == string.Empty)
+                {
+                    ShowMessage("Please enter data!");
+                }
+                else if (!status) textBox2.Text = MathOp.Multiply(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+                else
+                {
+                    textBox2.Text = MathOp.PercentageMult(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+                    status = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
             }
         }
 
@@ -65,32 +103,130 @@ namespace UWPSimpleCalculator
 
         private void btn_Div_Click(object sender, RoutedEventArgs e)
         {
-            if (!ch) textBox2.Text = operations.Division(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
-            else
+            try
             {
-                textBox2.Text = operations.PercentageDiv(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
-                ch = false;
+                if (textBox.Text == string.Empty || textBox.Text == string.Empty)
+                {
+                    ShowMessage("Please enter data!");
+                }
+                else if (!status) textBox2.Text = MathOp.Division(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+                else
+                {
+                    textBox2.Text = MathOp.PercentageDiv(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+                    status = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
             }
         }
 
         private void btn_Percentage_Click(object sender, RoutedEventArgs e)
         {
-            ch = true;
+            status = true;
         }
 
         private void btn_Sqrt_Click(object sender, RoutedEventArgs e)
         {
-            textBox2.Text = operations.Sqrt(double.Parse(textBox.Text)).ToString();
+            try
+            {
+                textBox2.Text = MathOp.Sqrt(double.Parse(textBox.Text)).ToString();
+                textBox1.Text = "";
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
         }
 
         private void btn_Pow_Click(object sender, RoutedEventArgs e)
         {
-            textBox2.Text = operations.Pow(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+            try
+            {
+                textBox2.Text = MathOp.Pow(double.Parse(textBox.Text), double.Parse(textBox1.Text)).ToString();
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
         }
 
         private void btn_Exp_Click(object sender, RoutedEventArgs e)
         {
-            textBox2.Text = operations.Exp(double.Parse(textBox.Text)).ToString();
+            try
+            {
+                textBox2.Text = MathOp.Exp(double.Parse(textBox.Text)).ToString();
+                textBox1.Text = "";
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+
+        private void btn_Fact_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                textBox2.Text = MathOp.Factorial(double.Parse(textBox.Text)).ToString();
+                textBox1.Text = "";
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+
+        private void btn_Sin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                textBox2.Text = MathOp.Sin(double.Parse(textBox.Text)).ToString();
+                textBox1.Text = "";
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+
+        private void btn_Cos_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                textBox2.Text = MathOp.Cos(double.Parse(textBox.Text)).ToString();
+                textBox1.Text = "";
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+
+        private void btn_Tan_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                textBox2.Text = MathOp.Tan(double.Parse(textBox.Text)).ToString();
+                textBox1.Text = "";
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+
+        private void btn_Clean_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
---
+++ UWPSimpleCalculator/MathOp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSimpleCalculator
+{
+    class MathOp
+    {
+        public static double Add(double FirstNumber, double secondNumber) => FirstNumber + secondNumber; 
+        public static double Subtract(double FirstNumber, double secondNumber) => FirstNumber - secondNumber; 
+        public static double Multiply(double FirstNumber, double secondNumber) => FirstNumber * secondNumber;
+        public static double Division(double FirstNumber, double secondNumber) => FirstNumber / secondNumber; 
+        public static double Pow(double Number, double Degree) => Math.Pow(Number, Degree);
+        public static double Sqrt(double Number) => Math.Sqrt(Number);
+        public static double Exp(double Number) => Math.Exp(Number);
+        public static double Sin(double Number) => Math.Sin(Number);
+        public static double Cos(double Number) => Math.Cos(Number);
+        public static double Tan(double Number) => Math.Tan(Number);
+        public static double PercentageAdd(double FirstNumber, double secondNumber) => FirstNumber + secondNumber / 100 * FirstNumber;
+        public static double PercentageSub(double FirstNumber, double secondNumber) => FirstNumber - secondNumber / 100 * FirstNumber;
+        public static double PercentageMult(double FirstNumber, double secondNumber) => Math.Pow(FirstNumber, 2) * secondNumber / (100 * FirstNumber);
+        public static double PercentageDiv(double FirstNumber, double secondNumber) => FirstNumber / (FirstNumber * secondNumber / 100);
+        public static double Factorial(double n) => n == 0 ? 1 : Factorial(n - 1) * n;
+    }
+}
